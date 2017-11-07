3f4acd2d54a1ac1f5d99c5219d78a69f028bde53
miku561@163.com <miku561@163.com>
2017-11-07 18:21:42 +0800

    Complete the mask layout

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,39 +1,86 @@
 <template>
-  <div class="header">
-      <div class="avatar">
-        <img width="64" height="64" :src="seller.avatar">
-      </div>
-      <div class="sell-info">
-        <p class="title">
-          <span class="tag"></span>
-          <span>{{ seller.name }}</span>
-        </p>
-        <p class="detail-info">
-          {{ seller.description }} / {{ seller.deliveryTime }}分钟送达
-        </p>
-        <p v-if="seller.supports" class="reword-info">
-          <span class="activity-signal" :class="classMap[seller.supports[0].type]"></span>
-          <span class="reword-detail">{{ seller.supports[0].description }}</span>
-        </p>
-      </div>
-      <div class="pub-ad">
-        <span class="ad-sign"></span>
-        <span>{{ seller.bulletin }}</span>
-        <i class="icon-keyboard_arrow_right"></i>
-      </div>
+    <div class="header">
+        <div class="avatar">
+          <img width="64" height="64" :src="seller.avatar">
+        </div>
+        <div class="sell-info">
+          <p class="title">
+            <span class="tag"></span>
+            <span>{{ seller.name }}</span>
+          </p>
+          <p class="detail-info">
+            {{ seller.description }} / {{ seller.deliveryTime }}分钟送达
+          </p>
+          <p v-if="seller.supports" class="reword-info">
+            <span class="activity-signal" :class="classMap[seller.supports[0].type]"></span>
+            <span class="reword-detail">{{ seller.supports[0].description }}</span>
+          </p>
+        </div>
+        <div class="pub-ad">
+          <span class="ad-sign"></span>
+          <span>{{ seller.bulletin }}</span>
+          <i class="icon-keyboard_arrow_right"></i>
+        </div>
 
-      <a href="#" class="num-title">
-        <span>5个</span>
-        <i class="icon-keyboard_arrow_right"></i>
-      </a>
-  </div>  
+        <a href="#" class="num-title" @click="showMask">
+          <span>{{ classMap.length }}个</span>
+          <i class="icon-keyboard_arrow_right"></i>
+        </a>
+        <div class="headerbg">
+          <img :src="seller.avatar">
+        </div>
+      <div class="maskLayout" v-show="maskFlag" transition="fade">
+        <div class="mask-wrap clearfix">
+          <div class="mask-content">
+            <p class="mask-title">{{ seller.name }}</p>
+            <div class="mask-score">
+              <star :size="48" :score="seller.score"></star>  
+            </div>
+            <div class="part-title">
+              <div class="line"></div>
+              <div class="title-name">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul class="part-info" v-if="seller.supports">
+              <li v-for="(val, index) in seller.supports" class="info-item">
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="info-text">{{ seller.supports[index].description}}</span>
+              </li>
+            </ul>
+             <div class="part-title">
+              <div class="line"></div>
+              <div class="title-name">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="part-bulletin">
+              <p class="text">{{ seller.bulletin }}</p>
+            </div>
+          </div>
+        </div>
+        <div class="mask-close" @click="showMask"><i class="icon-close"></i></div>
+      </div>
+    </div>  
 </template>
 
 <script>
+  import star from '@/components/star/star'
   export default {
     props: {seller: {type: Object}},
+    data () {
+      return {
+        maskFlag: false
+      }
+    },
+    methods: {
+      showMask () {
+        this.maskFlag = !this.maskFlag
+      }
+    },
     created () {
       this.classMap = [ 'decrease', 'discount', 'special', 'invoice', 'guarantee' ]
+    },
+    components: {
+      star
     }
   }
 </script>
@@ -42,7 +89,8 @@
   @import "../../common/sass/mixin.scss";
   .header{
     position: relative;
-    background: #889;
+    overflow: hidden;
+    background: rgba(7,17,27,0.5);
     color: #fff;
     .avatar{
       display: inline-block;
@@ -103,10 +151,9 @@
       }
     }
     .pub-ad{
-        position:   relative;
-        padding-right: 16px;
+        position:  relative;
+        padding:0 22px 0 12px;
         height: 28px;
-        width: 98%;
         text-overflow: ellipsis;
         overflow: hidden;
         white-space: nowrap;
@@ -125,23 +172,150 @@
         }
         i{
           position: absolute;
-          top:10px;
+          top:8px;
           right: 12px;
+          font-size: 10px;
         }
       }
     .num-title{
+            position: absolute;
+            right:16px;
+            bottom:40px;
+            padding:8px 10px;
+            border-radius: 12px;
+            line-height: 12px;
+            font-size: 10px;
+            color: #fff;
+            background: rgba(0, 0, 0, 0.2);
+            i{
+              vertical-align: text-bottom;
+            }
+    }
+    .headerbg{
       position: absolute;
-      right:16px;
-      bottom:40px;
-      padding:8px 10px;
-      border-radius: 12px;
-      line-height: 12px;
-      font-size: 10px;
-      color: #fff;
-      background: rgba(0, 0, 0, 0.2);
-      i{
-        vertical-align: text-bottom;
+      top:0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      filter: blur(10px);
+      img{
+        width: 100%;
+      }
+    }
+    .maskLayout{
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 100;
+      width: 100%;
+      height: 100%;
+      overflow: auto;
+      backdrop-filter:blur(10px);
+      transition: all 4s;
+      background: rgba(7,17,27,0.8);
+      &.fade-transition{
+        opacity: 1;
+        background: rgba(7,17,27,0.8);
+      }
+      &.fade-enter,&.fade-leave{
+        opacity: 0;
+        background: rgba(7,17,27,1);
+      }
+      .mask-wrap{
+        min-height: 100%;
+        width: 100%;
+        .mask-content{
+          margin-top: 64px;
+          padding-bottom: 64px;
+          .mask-title{
+            font-size: 16px;
+            line-height: 16px;
+            color: #fff;
+            font-weight: 700;
+            text-align: center;
+          }
+          .mask-score{
+            padding: 16px 0 28px;
+            text-align:center;
+          }
+          .part-title{
+            display: flex;
+            width: 80%;
+            margin: 0 auto 24px ;
+            .line{
+              flex: 1;
+              position: relative;
+              top: -6px;
+              border-bottom:1px solid rgba(255,255,255,0.2);
+            }
+            .title-name{
+              padding: 0 12px;
+              font-weight: 700;
+            }
+          }
+          .part-info{
+            width: 80%;
+            margin: 0 auto 28px;
+            .info-item{
+              padding: 0 12px;
+              margin-bottom: 12px;
+              font-size: 0;
+              &:last-child{
+                margin-bottom: 0;
+              }
+              .icon{
+                display: inline-block;
+                margin-right: 6px;
+                width: 16px;
+                height: 16px;
+                vertical-align: top;
+                background-repeat: no-repeat;
+                background-size: 16px 16px;
+                &.decrease{
+                  @include bg-media('decrease_2');
+                }
+                &.discount{
+                  @include bg-media('discount_2');
+                }
+                &.guarantee{
+                  @include bg-media('guarantee_2');
+                }
+                &.invoice{
+                  @include bg-media('invoice_2');
+                }
+                &.special{
+                  @include bg-media('special_2');
+                }
+              }
+              .info-text{
+                line-height: 16px;
+                font-size: 12px;
+              }
+            }
+          }
+          .part-bulletin{
+            margin: 0 auto;
+            width: 80%;
+            .text{
+              padding: 0 12px;
+              font-size: 12px;
+              font-weight: 200;
+              line-height: 24px;
+            }
+          }
+        }
+      }
+      .mask-close{
+          position: relative;
+          margin: -64px auto 0 auto;
+          width: 32px;
+          height: 32px;
+          font-size: 32px;
+          clear: both;  
+          color: rgba(255,255,255,0.5);
       }
     }
   }
+  
 </style>
---
+++ src/components/star/star.vue
@@ -0,0 +1,114 @@
+<template>
+	<div class="star" :class="starType">
+		<span v-for="itemClass in itemClasses" :class="itemClass" class="star-item"></span>
+	</div>
+</template>
+
+<script type="text/script">
+	const LENGTH = 5
+	const CLS_HALF = 'half'
+	const CLS_OFF = 'off'
+	const CLS_ON = 'on'
+	export default {
+  props: {
+    size: {
+      type: Number
+    },
+    score: {
+      type: Number
+    }
+  },
+  computed: {
+    starType () {
+      return 'star-' + this.size
+    },
+    itemClasses () {
+      let result = []
+      let score = Math.floor(this.score * 2) / 2
+      let hasDecimal = score % 1 !== 0
+      let integer = Math.floor(score)
+      for (let i = 0; i < integer; i++) {
+        result.push(CLS_ON)
+      }
+      if (hasDecimal) {
+        result.push(CLS_HALF)
+      }
+      while (result.length < LENGTH) {
+        result.push(CLS_OFF)
+      }
+      return result
+    }
+  }
+	}
+</script>
+
+<style lang="scss">
+	@import '../../common/sass/mixin.scss';
+	.star{
+		font-size: 0;
+		.star-item{
+			display: inline-block;
+			background-repeat: no-repeat;
+		}
+		&.star-48{
+			.star-item{
+				width: 20px;
+				height: 20px;
+				margin-right: 22px;
+				background-size: 20px 20px;
+				&:last-child{
+					margin-right: 0;
+				}
+				&.on{
+					@include bg-media('star48_on')
+				}
+				&.off{
+					@include bg-media('star48_off')
+				}
+				&.half{
+					@include bg-media('star48_half')
+				}
+			}
+		}
+		&.star-36{
+			.star-item{
+				width: 15px;
+				height: 15px;
+				margin-right: 6px;
+				background-size: 15px 15px;
+				&:last-child{
+					margin-right: 0;
+				}
+				&.on{
+					@include bg-media('star36_on')
+				}
+				&.off{
+					@include bg-media('star36_off')
+				}
+				&.half{
+					@include bg-media('star36_half')
+				}
+			}
+		}
+		&.star-24{
+			.star-item{
+				width: 10px;
+				height: 10px;
+				margin-right: 3px;
+				background-size: 10px 10px;
+				&:last-child{
+					margin-right: 0;
+				}
+				&.on{
+					@include bg-media('star24_on')
+				}
+				&.off{
+					@include bg-media('star24_off')
+				}
+				&.half{
+					@include bg-media('star24_half')
+				}
+			}
+		}
+	}
+</style>
